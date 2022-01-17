f9d8e5c376765c1ee174228f4cbb653257ad87a4
HeliumHe-He <78815047+HeliumHe-He@users.noreply.github.com>
2022-01-17 12:28:32 +0100

    idioti nesposobni gamad ko je smislio takav output zadah mu se zgadio

--- lab4_checke_GUI_v2.py
+++ lab4_checke_GUI_v2.py
@@ -358,6 +358,8 @@ def print(*args, start="", sep=' ', end="\n"+"_"*150+"\n"):
 	output["state"] = tk.DISABLED;
 pass;
 
+window.mainloop();
+
 lorem_ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula dolor tristique dictum luctus. Aliquam erat volutpat. 
 Aenean finibus bibendum ornare. Sed semper sem eget suscipit consectetur. Suspendisse at dui at nunc lobortis fermentum. 
 Nam congue mauris suscipit, rhoncus justo eget, ullamcorper nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
